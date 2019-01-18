194377e70f6df4f0b0ca65275b6176e955271783
Nathanael Weinrich <nathanaelweinrich@gmx.de>
2019-01-18 15:14:23 +0100

    fixed full stochastic. implemented advanced pdf for simple

--- RunMC.cxx
+++ RunMC.cxx
@@ -23,13 +23,17 @@ TNtuple* data_adv = new TNtuple("data_adv","data_adv","dt");
 
 //// singe pdf procedure
 // Setting up PDF
-double A = 1.95365e+03;
-double F = 8.81427e+01;
-double tau = 2.189e+03;
+double BG = 220;
+double N = 5.5e6;
+double tau = 2.193e+03;
+double lambdaC = 102.6e-6;
+double tauC = 1/lambdaC;
+double rho = 1.268;
 
-TF1 *f1 = new TF1("f", "[0] + [1]*exp(-x/[2])",200, 30000);
-f1->SetParNames("F", "A", "tau");
-f1->SetParameters(F, A, tau);
+
+TF1 *f1 = new TF1("f", "[0] + [1]*exp(-x/[2])*(1/[2] + (1/[2] + [3]) * exp(-x*[3])/[4] )",200, 30000);
+f1->SetParNames("BG","N","tau","lambdaC","rho");
+f1->SetParameters(BG,N,tau,lambdaC,rho);
 
 // Iteration
 double dt;
@@ -47,48 +51,62 @@ std::cout << "Processed events : " << NbEvt << std::endl;
 
 
 //// Full Stochastic procedure
-TF1 *pf1 = new TF1("pf1", "[0]*exp(-x/[1])",100, 30000);
-pf1->SetParNames("N", "tau");
-pf1->SetParameters(A, tau);
-TF1 *pf2 = new TF1("pf2", "[0]*exp(-x/[1])",100, 30000);
-pf2->SetParNames("N", "tau");
-pf2->SetParameters(A, tau);
+double tauP = 2197;
+double tauM = 1640;
+
+TF1 *pf1 = new TF1("pf1", "[0]*(1/[1]) * exp(-x/[1])",100, 30000);
+pf1->SetParNames("N", "tau+");
+pf1->SetParameters(1, tauP);
+TF1 *pf2 = new TF1("pf2", "[0]*(1/[1]) * exp(-x/[1])",100, 30000);
+pf2->SetParNames("N", "tauC");
+pf2->SetParameters(1, tauC);
 TRandom* RanGen = new TRandom();
+
+double lambda = 6.666666666666667e-05;
+
+TF1 *pf3 = new TF1("pf3", "[1] * exp(-x*[0])",50, 30000);
+pf2->SetParNames("lambda,N");
+pf2->SetParameters(lambda,1./lambda);
 
 
 // procedure
-double Ppm = 1.268/2; //muon+ to muon- ration
-double PSB = 0.7; //Signal to Background ration
-double Pcp = 0.05;
+double Ppm = rho/2; //muon+ / muon- ration
+double PSB = 0.59; //Signal to Background ration
 //
 
+int NP=0, NM=0, NB=0;
+
+double t_bkg, te, tC;
 for (NbEvt = 0; NbEvt < NbMax ; NbEvt++)
     {
     if(NbEvt%1000==0) std::cout << NbEvt << "\r" << std::flush;
 
-    if(((double)rand() / (double)RAND_MAX) >= PSB){
-        // Background
-        dt = RanGen->Uniform(100,30000);
+    t_bkg = RanGen->Uniform(000,30000);
+    if(((double)rand() / (double)RAND_MAX) > PSB){
+        // muon muon only
+        dt = t_bkg;
+        NB++;
     }
-    else if(((double)rand() / (double)RAND_MAX) < Ppm){
+    else if(((double)rand() / (double)RAND_MAX) > Ppm){
         // muon-
         dt = pf1->GetRandom();
+        dt = std::min(dt,t_bkg);
+        NM++;
     }
     else{
         // muon+
-        if(((double)rand() / (double)RAND_MAX) < Pcp){
-            // Muon capture
-            dt = RanGen->Uniform(100,30000);
-        }
-        else{
-            dt = pf2->GetRandom();
-        }
+        te = pf1->GetRandom();
+        tC = pf2->GetRandom();
+        dt = std::min(te ,std::min(tC ,t_bkg));
+        NP++;
     }
 
 
     data_adv->Fill(dt);
   }
 std::cout << "Processed events : " << NbEvt << std::endl;
+std::cout << "Background, Muon+, Muon-: " << NB << "\t" << NP<< "\t" << NM<< "\t" << std::endl;
+
 
 
 //// End
